03a6dfa6b4babd0be9e292d931b3e44dca2735da
Prashant-jumpbyte <prashant.b@appgambit.com>
2019-04-26 18:18:29 +0530

    add spec path

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   globalSetup: './setup.js',
   globalTeardown: './teardown.js',
   testEnvironment: './puppeteer_environment.js',
+  testMatch: [
+    // '**/e2e/**/itemhold-verify-hold.spec.js'
+    // '**/e2e/**/itemhold-release-item.spec.js'
+    '**/__tests__/**/*.spec.js'
+  ],
 }
